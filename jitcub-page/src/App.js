07b2c5a241515ef4413bf892b4177da38b4b070a
tommy.huang <tommy.huang@wavenet.com.tw>
2021-05-05 16:55:27 +0800

    delete original assets and add  App.css

--- jitcub-page/src/App.js
+++ jitcub-page/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
+import Navbar from './components/Navbar/Navbar'
+
+const StyledApp = styled.div`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Navbar></Navbar>
+    </StyledApp>
   )
 }
 
